f9ba6c2823b9e8699394cb8a07e7e89d86699e56
Ustyantsev_Daniil <gikata.38@yandex.ru>
2024-11-25 04:59:36 +0300

    ADD: tables and dto interface

--- core/model/dto.py
+++ core/model/dto.py
@@ -3,6 +3,7 @@ from typing import Optional, List
 
 
 class UserDTO(BaseModel):
+    user_id: int
     phone_number: str
     name: str
     second_name: str
@@ -10,3 +11,24 @@ class UserDTO(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class RoleDTO(BaseModel):
+    role_id: int
+    name: str
+
+    class Config:
+        from_attributes = True
+
+
+class UserRoleBase(BaseModel):
+    user_id: int
+    role_id: int
+
+
+class UserWithRoles(UserDTO):
+    roles: List[UserDTO]
+
+
+class RoleWithUsers(RoleDTO):
+    users: List[RoleDTO]
--- core/schemas/base.py
+++ core/schemas/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr, relationship
 
 from utils import camel_case_to_snake_case
@@ -13,8 +13,25 @@ class Base(DeclarativeBase):
 
 
 class User(Base):
+    user_id: Mapped[int] = mapped_column(unique=True, primary_key=True)
     phone_number: Mapped[str] = mapped_column(String(50), unique=True, primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
     second_name: Mapped[str] = mapped_column(String(50))
     last_name: Mapped[str] = mapped_column(String(50))
 
+    roles: Mapped[list['Role']] = relationship('Role', secondary='user_role', back_populates='users')
+
+
+class Role(Base):
+    role_id: Mapped[int] = mapped_column(unique=True, primary_key=True)
+    name: Mapped[str] = mapped_column(String(30), unique=True, primary_key=True)
+
+    users: Mapped[list['User']] = relationship('User', secondary='user_role', back_populates='roles')
+
+
+class UserRole(Base):
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.user_id'), primary_key=True)
+    role_id: Mapped[int] = mapped_column(ForeignKey('role.role_id'), primary_key=True)
+
+    user: Mapped['User'] = relationship('User', back_populates='roles')
+    role: Mapped['Role'] = relationship('Role', back_populates='users')
